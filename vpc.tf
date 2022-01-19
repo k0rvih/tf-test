e58fdb8b67c81400fae36688ae71d2964bfc2a99
Barbul Oleksandr <obarbul@primetrust.com>
2022-01-19 17:47:54 +0200

    added backend

--- vpc.tf
+++ vpc.tf
@@ -5,3 +5,16 @@ data "aws_vpc" "vpc_id" {
 output "vpc_id" {
   value = data.aws_vpc.vpc_id.id
 }
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "landingzone-s3-production-tfstate-automationhub"
+    key = "test001"
+    region = "us-east-1"
+    dynamodb_table = "global-terraform-lock"
+  }
+}
